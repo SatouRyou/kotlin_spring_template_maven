80d46ecb3be2c4476a9889e4a3cc87d68f661847
SatouRyou <satouryou1205@gmail.com>
2016-10-05 20:29:12 +0900

    サービスクラスとの連携

--- src/main/kotlin/com/kotlinspringtemplatemaven/controller/SampleController.kt
+++ src/main/kotlin/com/kotlinspringtemplatemaven/controller/SampleController.kt
@@ -1,7 +1,10 @@
 package com.kotlinspringtemplatemaven.controller
 
 import com.kotlinspringtemplatemaven.domain.dto.EmpDto
+import com.kotlinspringtemplatemaven.domain.service.SampleService
+import com.kotlinspringtemplatemaven.domain.trust.SampleTrustInterface
 import com.kotlinspringtemplatemaven.domain.vo.EmpVo
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -11,42 +14,43 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api")
 open class SampleController {
 
+    @Autowired
+    private lateinit var sampleService : SampleService
+
     @ResponseBody
     // Kotlinで通信方式を設定する場合には、arrayOfが必要
     @RequestMapping(method = arrayOf(RequestMethod.POST))
     fun getAll(): Array<EmpDto> {
-        val empDto = EmpDto( "test", 1 )
-        val empDtos = arrayOf( empDto )
-        return empDtos
+        return sampleService.getAll()
     }
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = arrayOf(RequestMethod.GET))
     fun get( @PathVariable id : Int) : EmpDto {
-        return EmpDto("name", id)
+        return sampleService.get( id );
     }
 
     @ResponseBody
     @RequestMapping(method = arrayOf(RequestMethod.POST))
     fun post( @RequestBody id :Int ) : EmpDto {
-        return  EmpDto("name", id)
+        return sampleService.get( id )
     }
 
     @ResponseBody
     @RequestMapping( value = "/save", method = arrayOf(RequestMethod.POST))
     fun post( @RequestBody empVo : EmpVo ) {
-
+        sampleService.save( empVo )
     }
 
     @ResponseBody
     @RequestMapping( value = "/save", method = arrayOf(RequestMethod.PUT) )
     fun put( @RequestBody empVo : EmpVo ) {
-
+        sampleService.save( empVo )
     }
 
     @ResponseBody
     @RequestMapping( value = "{id}", method = arrayOf(RequestMethod.DELETE) )
     fun delete( @PathVariable id: Int ) {
-
+        sampleService.delete( id )
     }
 }
